7b46db83ee8ec8c6c0833c32621d8be3a1ebcd3f
HI-TECHSTUDIO2024 <vladimir.yarovoy.jr@gmail.com>
2024-09-01 10:26:57 +0200

    Mega SASS Update
    Added Sass to the project

---
+++ test-site-sass/js.js
@@ -0,0 +1,4 @@
+// var headingElement = document.getElementById("main-heading");
+// console.log(headingElement.innerHTML);
+// var newHeadingText = prompt("Please provide with a new heading");
+// headingElement.innerHTML = newHeadingText;
